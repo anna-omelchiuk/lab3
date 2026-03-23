06779e4eedc94d65ecce78b8f44fb84df275b7bd
sentorch <anna.omelchiuk@gmail.com>
2026-03-23 20:55:27 +0200

    tab

--- lab3/Parallelogram.cs
+++ lab3/Parallelogram.cs
@@ -65,33 +65,33 @@ class Parallelogram
     }
 
     public void Diagonals(out double d1, out double d2)
-{
-    double rad = angle * Math.PI / 180;
-    double cos = Math.Cos(rad);
-    
-    d1 = Math.Sqrt(a * a + b * b - 2 * a * b * cos);
-    d2 = Math.Sqrt(a * a + b * b + 2 * a * b * cos);
-}
+    {
+        double rad = angle * Math.PI / 180;
+        double cos = Math.Cos(rad);
+        
+        d1 = Math.Sqrt(a * a + b * b - 2 * a * b * cos);
+        d2 = Math.Sqrt(a * a + b * b + 2 * a * b * cos);
+    }
 
-public void Heights(out double h1, out double h2)
-{
-    double rad = angle * Math.PI / 180;
-    double s = Math.Sin(rad);
-    
-    h1 = b * s;
-    h2 = a * s;
-}
+    public void Heights(out double h1, out double h2)
+    {
+        double rad = angle * Math.PI / 180;
+        double s = Math.Sin(rad);
+        
+        h1 = b * s;
+        h2 = a * s;
+    }
 
-public void PrintInfo(string name)
-{
-    Diagonals(out double d1, out double d2);
-    Heights(out double h1, out double h2);
+    public void PrintInfo(string name)
+    {
+        Diagonals(out double d1, out double d2);
+        Heights(out double h1, out double h2);
 
-    Console.WriteLine($"\nПАРАЛЕЛОГРАМ {name}");
-    Console.WriteLine($"a = {GetA()}; b = {GetB()}; кут = {GetAngle()}");
-    Console.WriteLine($"Периметр: {Perimeter()}");
-    Console.WriteLine($"Площа: {Area():F2}");
-    Console.WriteLine($"Діагоналі: {d1:F2}; {d2:F2}");
-    Console.WriteLine($"Висоти: {h1:F2}; {h2:F2}");
-}
+        Console.WriteLine($"\nПАРАЛЕЛОГРАМ {name}");
+        Console.WriteLine($"a = {GetA()}; b = {GetB()}; кут = {GetAngle()}");
+        Console.WriteLine($"Периметр: {Perimeter()}");
+        Console.WriteLine($"Площа: {Area():F2}");
+        Console.WriteLine($"Діагоналі: {d1:F2}; {d2:F2}");
+        Console.WriteLine($"Висоти: {h1:F2}; {h2:F2}");
+    }
 }
