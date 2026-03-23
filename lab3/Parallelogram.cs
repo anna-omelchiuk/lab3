7887428985a07cee12f4e7673420bc506e1d40d1
sentorch <anna.omelchiuk@gmail.com>
2026-03-23 18:58:09 +0200

    Добавьте файлы проекта.

---
+++ lab3/Parallelogram.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+class Parallelogram
+{
+    private double a;
+    private double b;
+    private double angle;
+
+    public Parallelogram()
+    {
+        a = 0;
+        b = 0;
+        angle = 0;
+    }
+
+    public Parallelogram(double a, double b, double angle)
+    {
+        this.a = a;
+        this.b = b;
+        this.angle = angle;
+    }
+
+    public void SetA(double value)
+    {
+        if (value >= 0)
+            a = value;
+    }
+
+    public void SetB(double value)
+    {
+        if (value >= 0)
+            b = value;
+    }
+
+    public void SetAngle(double value)
+    {
+        if (value >= 0 && value < 180)
+            angle = value;
+    }
+
+    public double GetA()
+    {
+        return a;
+    }
+
+    public double GetB()
+    {
+        return b;
+    }
+
+    public double GetAngle()
+    {
+        return angle;
+    }
+
+    public double Perimeter()
+    {
+        return 2 * (a + b);
+    }
+
+    public double Area()
+    {
+        double rad = angle * Math.PI / 180;
+        return a * b * Math.Sin(rad);
+    }
+
+    public (double d1, double d2) Diagonals()
+    {
+        double rad = angle * Math.PI / 180;
+        double cos = Math.Cos(rad);
+        double d1 = Math.Sqrt(a * a + b * b - 2 * a * b * cos);
+        double d2 = Math.Sqrt(a * a + b * b + 2 * a * b * cos);
+        return (d1, d2);
+    }
+
+    public (double h1, double h2) Heights()
+    {
+        double rad = angle * Math.PI / 180;
+        double s = Math.Sin(rad);
+        return (b * s, a * s);
+    }
+
+    public void PrintInfo(string name)
+    {
+        var d = Diagonals();
+        var h = Heights();
+
+        Console.WriteLine($"\nПАРАЛЕЛОГРАМ {name}");
+        Console.WriteLine($"a = {GetA()}; b = {GetB()}; кут = {GetAngle()}");
+        Console.WriteLine($"Периметр: {Perimeter()}");
+        Console.WriteLine($"Площа: {Area():F2}");
+        Console.WriteLine($"Діагоналі: {d.d1:F2}; {d.d2:F2}");
+        Console.WriteLine($"Висоти: {h.h1:F2}; {h.h2:F2}");
+    }
+}
---
+++ lab3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Text;
+
+class Program
+{
+    public static double ReadDouble(string message)
+    {
+        double value;
+
+        while (true)
+        {
+            Console.Write(message);
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out value))
+                return value;
+
+            Console.WriteLine("\nВведіть коректне число.");
+        }
+    }
+
+    public static int ReadInt(string message)
+    {
+        int value;
+
+        while (true)
+        {
+            Console.Write(message);
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out value))
+                return value;
+
+            Console.WriteLine("\nПомилка! Введіть ціле число.");
+        }
+    }
+
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Parallelogram p1 = new Parallelogram();
+        Parallelogram p2 = new Parallelogram(6,3,60);
+
+        int choice;
+
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("\nМЕНЮ\n");
+                Console.WriteLine("1. Показати P1 і P2");
+                Console.WriteLine("2. Ввести дані для P1");
+                Console.WriteLine("3. Ввести дані для P2");
+                Console.WriteLine("0. Вихід");
+
+                choice = ReadInt("Ваш вибір: ");
+
+                switch (choice)
+                {
+                    case 1:
+                        p1.PrintInfo("A");
+                        p2.PrintInfo("B");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nВвід для P1:");
+                        p1.SetA(ReadDouble("Сторона a: "));
+                        p1.SetB(ReadDouble("Сторона b: "));
+                        p1.SetAngle(ReadDouble("Кут: "));
+                        break;
+
+                    case 3:
+                        Console.WriteLine("\nВвід для P2:");
+                        p2.SetA(ReadDouble("Сторона a: "));
+                        p2.SetB(ReadDouble("Сторона b: "));
+                        p2.SetAngle(ReadDouble("Кут: "));
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Вихід");
+                        return;
+
+                    default:
+                        Console.WriteLine("\nНевірний вибір!");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nСталася помилка: " + ex.Message);
+            }
+        }
+    }
+}
